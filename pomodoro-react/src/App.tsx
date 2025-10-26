781deae752aa3c284c951f8f54e0cee8efb95aab
anaberst <anavberst@gmail.com>
2025-10-26 15:39:19 -0700

    Add basic component functions

--- pomodoro-react/src/App.tsx
+++ pomodoro-react/src/App.tsx
@@ -1,5 +1,17 @@
+import React from "react";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <h2
+        className="container d-flex justify-content-center align-items-center"
+        style={{ minHeight: "20vh" }}
+      >
+        Pomodoro Timer
+      </h2>
+      <div></div>
+    </>
+  );
 };
 
 export default App;
