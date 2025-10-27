a74ee94cfd89e1bd8f49a17d1f5fef42b55c73eb
anaberst <anavberst@gmail.com>
2025-10-27 15:38:41 -0700

    Add in comments for next steps

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -6,12 +6,11 @@ import Timer from "./components/Timer";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const App = () => {
-  // "success" | "info" | "light" | "danger" | "secondary";
-  // start | pause | restart | yes, I'm sure | go back
-
   const startColor = "success";
   const pauseColor = "info";
   const restartColor = "light";
+  // const confirmColor = "danger"; this triggers restart AND makes modal disappear
+  // const undoColor = "secondary"; this simply makes the modal disappear
 
   const startText = "Start";
   const pauseText = "Pause";
@@ -45,6 +44,8 @@ const App = () => {
     setTimeLeft(Date.now() + minutes * 60000);
   };
 
+  // add in disabled color for restart button
+  // add in inspirationalText component
   return (
     <>
       <h2
